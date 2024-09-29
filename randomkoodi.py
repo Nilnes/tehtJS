b2c65f89d22febf3f4e764c538048e3ce956f4bf
juhay-git <juha.yli-hemminki@seamk.fi>
2024-09-29 20:16:43 +0300

    linkki lisätty

--- randomkoodi.py
+++ randomkoodi.py
@@ -4,7 +4,8 @@ import random
 #Lista linkkejä
 lista = ["https://www.youtube.com/watch?v=dtER80sOjX4",
         "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-        "https://www.youtube.com/watch?v=PKQPey6L42M",]
+        "https://www.youtube.com/watch?v=PKQPey6L42M",
+        "https://www.youtube.com/watch?v=DkWDHzmMvyg",]
 
 #Random valinta listasta
 url = (random.choice(lista))
